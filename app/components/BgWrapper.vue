03a212c8c0fb9fdf9bf2237492521432657de3c4
Fayzullo <fayzullarakhmatullayev@gmail.com>
2025-08-25 01:10:59 +0500

    fix: add key binding to NuxtImg for dynamic image updates and simplify image loading logic

--- app/components/BgWrapper.vue
+++ app/components/BgWrapper.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="bg-wrapper">
-    <NuxtImg alt="bg" :src="imageSrc" format="webp" quality="90" class="bg-img" />
+    <NuxtImg :key="route.fullPath" alt="bg" :src="imageSrc" format="webp" quality="90" class="bg-img" />
   </div>
 </template>
 
 <script lang="ts" setup>
 const route = useRoute();
 
-const images = ["/images/home-bg.jpg", "/images/all-bg.jpg"];
-
 const isIncludes = (pageName: string) => {
   return String(route.meta.page)?.includes(pageName);
 };
 
-const imageSrc = computed(() => (isIncludes("home") ? images[0] : images[1]));
-
-onMounted(() => {
-  images.forEach(src => {
-    const img = new Image();
-    img.src = src;
-  });
-});
+const imageSrc = computed(() => (isIncludes("home") ? "/images/home-bg.jpg" : "/images/all-bg.jpg"));
 </script>
 
 <style>
@@ -35,5 +26,14 @@ onMounted(() => {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
