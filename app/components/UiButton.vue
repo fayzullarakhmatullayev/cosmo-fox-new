eb0c6a7baeebdca1db1e8c84191c49aed61b9774
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-08-26 15:30:12 +0500

    token desktop done

--- app/components/UiButton.vue
+++ app/components/UiButton.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts" setup>
 const { variant = "small", type = "button" } = defineProps<{
-  variant?: "small" | "large";
+  variant?: "small" | "large" | "medium";
   type?: "button" | "submit" | "reset";
 }>();
 </script>
@@ -17,7 +17,7 @@ a {
   color: rgba(7, 30, 53, 1);
   padding: 13px 38px 14px;
   min-width: 182px;
-  background: linear-gradient(135deg, #69fee6, #23c8e0);
+  background: linear-gradient(135deg, #4dfcd2, #0ab4e6);
   text-align: center;
   display: block;
   cursor: pointer;
@@ -32,6 +32,16 @@ a {
 
     @include laptop {
       padding: 20px 70px 22px 68px;
+      font-size: 25px;
+    }
+  }
+
+  &.medium {
+    padding: 18px 45px 20px;
+    font-size: 30px;
+
+    @include laptop {
+      padding: 15px 35px 17px;
       font-size: 25px;
     }
   }
--- app/pages/token.vue
+++ app/pages/token.vue
@@ -1,9 +1,31 @@
 <template>
-  <h2>Token</h2>
+  <div class="token">
+    <h2 class="title">токен zylo</h2>
+
+    <div class="token__wrapper">
+      <div v-for="card in cards" :key="card.image" class="token__card">
+        <div class="token__card--content">
+          <nuxt-img :src="card.image" alt="token-img" quality="60" format="webp" class="token__card--image" />
+          <h2 class="token__card--text">{{ t(card.text) }}</h2>
+        </div>
+        <nuxt-img class="token__card--bg" src="/images/token-slot.png" alt="token" quality="60" format="webp" />
+      </div>
+    </div>
+    <ui-button variant="medium" class="token__button" href="#">
+      <span>white paper <icon-link /></span>
+    </ui-button>
+  </div>
 </template>
 
 <script lang="ts" setup>
 const { t } = useI18n();
+
+const cards = computed(() => [
+  { image: "/images/token-1.png", text: "token.text1" },
+  { image: "/images/token-2.png", text: "token.text2" },
+  { image: "/images/token-3.png", text: "token.text3" },
+  { image: "/images/token-4.png", text: "token.text4" }
+]);
 useHead({
   title: t("nav.token"),
   meta: [{ name: "description", content: "Token page" }]
