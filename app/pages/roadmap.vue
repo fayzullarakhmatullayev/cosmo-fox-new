a6b965408ae4b7e858b4a3c6b489a132ed9a7777
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-08-28 17:45:11 +0500

    radomap desktop done

--- app/pages/roadmap.vue
+++ app/pages/roadmap.vue
@@ -5,16 +5,28 @@
       <div
         v-for="(item, i) in roadmap"
         :key="i"
-        :class="['roadmap-item', i % 2 === 0 ? 'top' : 'bottom', `planet--${i + 1}`]"
+        :class="['roadmap-item', i % 2 === 0 ? 'top' : 'bottom', `planet-${i + 1}`]"
       >
         <div class="planet-wrapper">
-          <nuxt-img :src="item.planet" alt="planet" quality="80" format="webp" class="planet"></nuxt-img>
-        </div>
-        <div class="card">
-          <h3>{{ item.date }}</h3>
-          <p>{{ item.text }}</p>
+          <div class="planet-card">
+            <h3>{{ item.date }}</h3>
+            <p>{{ item.text }}</p>
 
-          <nuxt-img src="/images/roadmap/roadmap-slot.png" alt="line" quality="80" format="webp" class="roadmap-slot" />
+            <nuxt-img
+              src="/images/roadmap/roadmap-slot.png"
+              alt="line"
+              quality="80"
+              format="webp"
+              class="roadmap-slot"
+            />
+          </div>
+          <div class="planet-image">
+            <nuxt-img :src="item.planet" alt="planet" quality="80" format="webp" class="planet"></nuxt-img>
+          </div>
+
+          <div v-if="item.line" class="planet-line">
+            <nuxt-img :src="item.line" quality="80" format="webp" />
+          </div>
         </div>
       </div>
     </div>
@@ -33,7 +45,7 @@ const roadmap = computed(() => [
   },
   {
     date: "Summer 2025",
-    text: "Community & Early Access",
+    text: "Community &\n Early Access",
     planet: "/images/roadmap/planet-2.png",
     line: "/images/roadmap/line-2.png"
   },
@@ -45,7 +57,7 @@ const roadmap = computed(() => [
   },
   {
     date: "Fall 2025",
-    text: "Drill Race Evolution",
+    text: "Drill Race\n Evolution",
     planet: "/images/roadmap/planet-4.png",
     line: "/images/roadmap/line-4.png"
   },
@@ -57,26 +69,26 @@ const roadmap = computed(() => [
   },
   {
     date: "Feb 2026",
-    text: "Token Launch & Listing",
+    text: "Token Launch &\n Listing",
     planet: "/images/roadmap/planet-6.png",
     line: "/images/roadmap/line-6.png"
   },
   {
     date: "Feb 2026",
-    text: "Airdrop Distribution",
+    text: "Airdrop\n Distribution",
     planet: "/images/roadmap/planet-7.png",
     line: "/images/roadmap/line-7.png"
   },
   {
     date: "Q1 2026",
-    text: "Phase 2 Game Launch",
+    text: "Phase 2 Game\n Launch",
     planet: "/images/roadmap/planet-8.png",
     line: "/images/roadmap/line-8.png",
     width: 169
   },
   {
     date: "2026+",
-    text: "Ecosystem Growth & Governance",
+    text: "Ecosystem Growth\n & Governance",
     planet: "/images/roadmap/planet-9.png",
     line: null,
     width: 286
